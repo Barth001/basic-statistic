1c1707a59b74ccef7825949eb3eb65c27ac210aa
Barth001 <alaekwechekwube@gmail.com>
2022-12-11 15:42:09 -0800

    First commit

---
+++ statistics.js
@@ -0,0 +1,84 @@
+class Statistics {
+
+    constructor(...datas){
+        this.datas = datas
+    }
+
+    // Counting the total number given
+    nTotal(){
+        if(this.datas == null) return 0;
+        let count = 0;
+        for (let i = 0; i < this.datas.length; i++)
+            count++;
+        return count
+    }
+
+    // Summing the given data
+    sumNumber(){
+        if(this.datas == null) return 0;
+        let sum = 0;
+        for (let i = 0; i < this.datas.length; i++)
+            sum += this.datas[i];
+        return sum;
+    }
+
+    // counting number of occurance of each item
+    mostOccuringValue(){
+        if(this.datas == null) return null;
+        let modeDict = {}, modeArray = [], maxCount = 1
+        for(let i = 0; i < this.datas.length; i++){
+            var value = this.datas[i];
+        if(modeDict[value] == null) modeDict[value] = 1;
+        else modeDict[value]++;
+
+        if(modeDict[value] > maxCount){
+            modeArray = [value]
+            maxCount = modeDict[value]
+        } else if (modeDict[value] == maxCount){
+            modeArray.push(value);
+            maxCount = modeDict[value];
+        }
+      }
+      return modeArray;
+    }
+
+    sortArray(){
+        return this.datas.sort();
+    }
+
+    mode(){
+        let number = this.mostOccuringValue();
+        console.log(`The mode is: ${number}`);
+    }
+
+    mean(){
+        let n = this.nTotal(), sum =this.sumNumber();
+        console.log(sum / n);
+    }
+
+    median(){
+        let n = this.nTotal();
+        if(n % 2 == 0){
+            let result = ((n / 2) + ((n / 2) + 1)) / 2
+            console.log(`Median is: ${result}`);
+        } else {
+            let result = ((n +1) / 2)
+            console.log(`Median is: ${result}`);
+        }
+    }
+
+    range(){
+        let arr = this.sortArray();
+        let result = arr[arr.length - 1] - arr[0];
+        console.log(`The range is: ${result}`);
+    }
+
+    variance(){}
+    standardDeviation(){}
+}
+
+const sta = new Statistics(1,3,3,4,2,2);
+sta.mean();
+sta.mode();
+sta.median();
+sta.range()
